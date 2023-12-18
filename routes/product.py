36d0a1f08e1bcf5db2583e1a70f89f155803ea0a
moonjiye <mjy6088@gmail.com>
2023-12-18 09:34:04 +0900

    edit content

--- routes/product.py
+++ routes/product.py
@@ -4,8 +4,9 @@ from bs4 import BeautifulSoup
 
 
 def get_product():
-    merch_info = []
+    product_info = []
     product_urls = [
+        # gbbul_nocharim
         'https://marpple.shop/kr/gbbul_nocharim/products/14154311',
         'https://marpple.shop/kr/gbbul_nocharim/products/14154317',
         'https://marpple.shop/kr/gbbul_nocharim/products/14154327',
@@ -18,32 +19,36 @@ def get_product():
         'https://marpple.shop/kr/gbbul_nocharim/products/14154008',
         'https://marpple.shop/kr/gbbul_nocharim/products/14153869',
         'https://marpple.shop/kr/gbbul_nocharim/products/14130904',
-        'https://marpple.shop/kr/gbbul_nocharim/products/14130600'
+        'https://marpple.shop/kr/gbbul_nocharim/products/14130600',
+        # tvt
+        'https://marpple.shop/kr/tvt/products/9022966',
+        'https://marpple.shop/kr/tvt/products/10872638',
+        'https://marpple.shop/kr/tvt/products/10872575',
+        'https://marpple.shop/kr/tvt/products/10872588',
+        'https://marpple.shop/kr/tvt/products/10872594',
+        'https://marpple.shop/kr/tvt/products/10872604',
+        'https://marpple.shop/kr/tvt/products/9022970',
+        'https://marpple.shop/kr/tvt/products/9022975',
+        'https://marpple.shop/kr/tvt/products/7849624',
+        'https://marpple.shop/kr/tvt/products/7863551',
+        'https://marpple.shop/kr/tvt/products/7863398'
     ]
+
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
     }
 
-
-    # 하위 URL 리스트
-
-    # 상위 URL에서 카테고리 이름 추출
-    # base_url = 'https://marpple.shop/kr/gbbul_nocharim/'
-    # response = requests.get(base_url)
-    # soup = BeautifulSoup(response.content, 'html.parser')
-    # category_names = soup.find_all('div', class_='app-product-item__category-name')
-    # for category in category_names:
-    #     print(category.text.strip())
-    #
-
-    # for link in category_names:
-    #     product_url = link.get('href')
-
-    # 하위 URL에서 제품 정보 추출
     for url in product_urls:
+        parts = url.split('/')
         try:
+            name_index = parts.index('kr') + 1
+            if name_index < len(parts):
+                artist_name = parts[name_index]
+            else:
+                print("단어를 찾을 수 없습니다.")
+
             response = requests.get(url, headers=headers)
-            print(f"status code : {response.status_code}")
+            # print(f"status code : {response.status_code}")
             soup = BeautifulSoup(response.text, 'html.parser')
 
             # 제품 정보 추출
@@ -51,8 +56,8 @@ def get_product():
             product_image = soup.find('img', {'class': 'mp-product-img'})['src']
             product_price = soup.find('span', {'class': 'pd-product__price'}).get_text(strip=True)
 
-            merch_info.append({
-                'url': url,
+            product_info.append({
+                'artist': artist_name,
                 'name': product_name,
                 'image': product_image,
                 'price': product_price
@@ -63,9 +68,9 @@ def get_product():
 
 
     # JSON으로 변환 및 출력
-    json_data = json.dumps(merch_info, ensure_ascii=False, indent=4)
+    json_data = json.dumps(product_info, ensure_ascii=False, indent=4)
     print(json_data)
-    for i in merch_info:
+    for i in product_info:
         print(i)
     return json_data
 
